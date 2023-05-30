6b0bdeefcd2b691d6680fba3ad1322970521e15b
DVTyuriy <95183732+DVTyuriy@users.noreply.github.com>
2023-05-30 22:45:15 +0300

    32
    2323

---
+++ CPP_Projects/01010103.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{   //print figure to the screen
+
+    cout << "                       *\n" << endl;
+    cout << "                   *       *\n" << endl;
+    cout << "               *       *       *\n" << endl;
+    cout << "           *       *       *       *\n" << endl;
+    return 0;
+}
