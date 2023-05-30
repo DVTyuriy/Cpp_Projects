54301c416e5d836eb737f8bd61b6bc92ba9a0b0f
DVTyuriy <95183732+DVTyuriy@users.noreply.github.com>
2023-05-30 23:21:05 +0300

    wwe
    wewe

---
+++ 01010103/01010103.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+
+{   //print figure to the screen
+
+    cout << "                       *\n" << endl;
+    cout << "                   *       *\n" << endl;
+    cout << "               *       *       *\n" << endl;
+    cout << "           *       *       *       *\n" << endl;
+    return 0;
+}
