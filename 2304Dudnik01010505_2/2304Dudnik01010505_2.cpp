d2875f0ba3eb83bd84a3559e21897114e7ab5a2f
DVTyuriy <95183732+DVTyuriy@users.noreply.github.com>
2023-06-06 19:55:21 +0300

    main
    fix

---
+++ 2304Dudnik01010505_2/2304Dudnik01010505_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "The path of the turtle" << endl;
+    int enter_day;
+    cout << "How many days will the turtle crawl: ";
+    cin >> enter_day;
+    float commonway = 0, day_way = 1.5f;
+
+    for (int i = 0; i < enter_day; i++){
+        commonway += day_way;
+        day_way += 0.2f;
+    }
+    cout << "The turtle will crawl: "<< commonway<<endl;
+
+    return 0;
+}
--- TempVS/TempVS.cpp
+++ TempVS/TempVS.cpp
@@ -1,52 +1,86 @@
 ﻿#include <iostream>
-#include <Windows.h>
+#include <string>
+#include <cstring>
+#include <cmath>
+
 using namespace std;
+void divide(float a, float b) {
+    if (b == 0) throw 100; //если ошибка выбрасывает любое число как код ошибки
+    else cout << (a / b);
+}
+
 
 int main()
 {   //print list questions to the screen
-        SetConsoleCP(1251);
-        SetConsoleOutputCP(1251);
-        int enter_num, dev_num;
-        string fail = "I can't count negative number of days or zero";
+        //setlocale(LC_ALL, "UKRAINIAN");
+        //setlocale(LC_ALL, "RU");
 
-        cout << "Hello, welcome to the programme where you can get the number for month in year" << endl;
-        cout << "Enter the day number: ";
-        cin >> enter_num;
-
-        if (enter_num > 365)
-        {
-            dev_num = enter_num / 365;
-                     enter_num = enter_num % 365;
-            enter_num += dev_num;
-            cout << "The number day in year: " << enter_num << "\n" ;
+        try {
+            divide(5.2f, 0.0f);
         }
-        cout << "The number month is: ";
-        if (enter_num <= 0)
-            cout << fail;
-        else if (enter_num > 0 && enter_num <= 31)
-            cout << "1";
-        else if (enter_num > 31 && enter_num <= 59)
-            cout << "2";
-        else if (enter_num > 59 && enter_num <= 90)
-            cout << "3";
-        else if (enter_num > 90 && enter_num <= 120)
-            cout << "4";
-        else if (enter_num > 120 && enter_num <= 151)
-            cout << "5";
-        else if (enter_num > 151 && enter_num <= 181)
-            cout << "6";
-        else if (enter_num > 181 && enter_num <= 212)
-            cout << "7";
-        else if (enter_num > 212 && enter_num <= 243)
-            cout << "8";
-        else if (enter_num > 243 && enter_num <= 273)
-            cout << "9";
-        else if (enter_num > 273 && enter_num <= 304)
-            cout << "10";
-        else if (enter_num > 304 && enter_num <= 334)
-            cout << "11";
-        else if (enter_num > 334 && enter_num <= 365)
-            cout << "12";
+        catch (int err) {
+            if (err == 100) cout << "Ошибка при делении на 0" << endl;
+        }
+
+        string str1 = "Hello";
+        string str2 = "World";
+
+        str1.append(str2);
+        str1.pop_back(); //удаляет последний символ
+        str1.push_back('!'); //добавляет последний символ
+        str1.resize(5); //изменяет длину строки
+        cout << str1 << " - " << str1.length(); //выдает длину строки
+
+        cout << ceil(1.78f) << endl; //округляет к большему
+        cout << floor(1.78f) << endl; //округляет к меньшему
+        cout << round(1.78f) << endl; //математическое округление
 
         return 0;
+}
+
+
+// Шаблоны функций
+#include <iostream>
+#include <string>
+
+using namespace std;
+template <typename T, typename T2>
+void print_arr(T* arr, int len) {
+    for (T2 i = 0; i < len; i++)
+        cout << *(arr + i) << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr1[] = { 5, 6 , 7 , 8, -2, 1 };
+    print_arr<int, int>(arr1, 6);
+
+    float arr2[] = { 0.5f, 0.78f, 1.98f };
+    print_arr<float, int>(arr2, 3);
+
+}
+
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    int random_number;
+    random_number = rand();
+    cout << random_number << " - псевдослучайное число\n";
+
+    /*  
+Налаштування диапазону для генератора
+Если нужно генерировтаь числа от 0 до 10 или от 0 до 100?!то используем:
+int a=rand()%25;
+Если нужен диапазон другой, например от 11 до 16. Все просто. Число генерируемых от 0 до 5
+(разница между 16 и 11), а затем «сдвинуть» результат на 11.
+int a=rand()%6+11;
+Аналогично можно получить другие диапазоны
+
+    */
 }
