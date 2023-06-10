967cbaab99255433527bc1bf794673bc50836d98
DVTyuriy <95183732+DVTyuriy@users.noreply.github.com>
2023-06-10 21:18:31 +0300

    232
    232

--- 2304Dudnik01030805/2304Dudnik01030805.cpp
+++ 2304Dudnik01030805/2304Dudnik01030805.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <array>
 #include <algorithm>
+#include <time.h>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
+    int k;
     cout << "Welcome" << endl;
     array <int, 15> test_array = { -1, 0, 1, -2, 2, -3, 3, 4, -4, 6, -10, 15, 99, 5, -36 };
+    
     //cout << test_array.size() << endl;
     int min = test_array[0];
     int max = test_array[0];
-
-    for (int i = 0; i < 15; i++)
+    int n = sizeof(test_array) / sizeof(test_array[0]);
+    cout << "Length massive is: " << n << endl;
+    for (int i = 0; i < n; i++)
         if (test_array[i] > max) 
             max = test_array[i];
         else if (test_array[i] < min)
